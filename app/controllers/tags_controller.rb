444987bc7743090bbbc029a0406799d7f58923ee
NBarnes <nbarnes@gmail.com>
2014-02-23 16:25:35 -0800

    show and index for tags implemented.  Tag listings link to their page

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -1,2 +1,12 @@
 class TagsController < ApplicationController
+
+  def index
+    @tags = ActsAsTaggableOn::Tag.all
+  end
+
+  def show
+    @tag_name = params[:tag_name]
+    @photos = Photo.tagged_with(@tag_name)
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,8 @@ PhotoManager::Application.routes.draw do
 
   root :to => 'photos#display'
 
+  get 'tags', to: 'tags#index'
+  get 'tags/:tag_name', to: 'tags#show'
+
   mount PostgresqlLoStreamer::Engine => "/photo_image"
 end
