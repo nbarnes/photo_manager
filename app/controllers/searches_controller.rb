bd2510a12997dadb4b9e1d6c6bc7cb4db9bf1df7
NBarnes <nbarnes@gmail.com>
2014-02-24 11:52:47 -0800

    Search page improvements

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -3,12 +3,12 @@ class SearchesController < ApplicationController
   def search
     @search_term = params[:search]
 
-    @photos = Photo.where("title LIKE ?", "%#{ @search_term }%")
-    @galleries = Gallery.where("name LIKE ?", "%#{ @search_term }%")
-    @tags
-  end
+    @photos = Photo.where("title ILIKE ?", "%#{ @search_term }%")
+    @galleries = Gallery.where("name ILIKE ?", "%#{ @search_term }%")
 
-  def show
+    @photos_with_tag = Photo.tagged_with(@search_term)
+
+
   end
 
 end
